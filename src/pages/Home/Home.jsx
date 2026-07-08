6c7c12fafa73ad10d4281bde19a45b1892c44fec
Aryan Shrestha <aryanshrestha0430@gmail.com>
2026-07-08 12:02:28 +0545

    add fetch products

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,38 @@
 import './Home.css'
 import Navbar from "./components/Navbar/Navbar";
+import axios from 'axios'
+import { useState, useEffect } from "react";
 
- const Home = () => {
+
+const Home = () => {
+
+const [products, setProducts] = useState([]);
+
+
+    const fetchProducts = async () => {
+       const response = await axios.get('https://6a4cb746e1cf82a4a17d7572.mockapi.io/products')
+       console.log(response.data);
+       setProducts(response.data);
+    };
+
+    useEffect(() => {
+        fetchProducts();
+        // const fetchData = async () => {
+        //     try {
+        //         const response = await axios.get('https://api.example.com/products');
+        //         console.log(response.data);
+        //     } catch (error) {
+        //         console.error('Error fetching data:', error);
+        //     }
+        // };
+        // fetchData();
+    }, []);
+    
+
   return (
     <>
     <Navbar />
-        <div className="card">
+        {/* <div className="card">
         <img src="https://images.unsplash.com/photo-1542291026-7eec264c27ff?w=500" alt="Product"/>
 
         <div className="card-content">
@@ -18,9 +45,31 @@ import Navbar from "./components/Navbar/Navbar";
             <button>View Product</button>
         </div>
   
+    </div> */}
+
+
+{products.map((product) => {
+  return (
+    <div className="card" key={product.id}>
+      {/* <img src={product.productImage} alt={product.productName} /> */}
+
+      <div className="card-content">
+        <h2>{product.productName}</h2>
+                <img src={product.productImage} alt={product.productName} />
+
+
+
+
+        <p>{product.productDescription}</p>
+
+        <button>View Product</button>
+      </div>
     </div>
-    </>
+  );
+})}
+    </>  
   )
 }
+
 
 export default Home
